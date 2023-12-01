ff1aaa86a01686a1721ec03fd736e06169d592e9
My Name <tadzhibaevaelnura@gmail.com>
2023-12-01 23:37:44 +0600

    Recursion

---
+++ src/Recursion.java
@@ -0,0 +1,22 @@
+public class Recursion {
+
+    public static void main(String[] args) {
+        greet("adit");
+
+    }
+
+    private static void greet2(String name) {
+        System.out.println("how are you, " + name + "?");
+    }
+
+    private static void bye() {
+        System.out.println("ok bye!");
+    }
+
+    private static void greet(String name) {
+        System.out.println("hello, " + name + "!");
+        greet2(name);
+        System.out.println("getting ready to say bye...");
+        bye();
+    }
+}
